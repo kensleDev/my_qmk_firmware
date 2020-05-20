49a0522656d6b995065ebec1a4ec8d30a73aedbf
kensleDev <kensledev@gmail.com>
2020-05-20 23:32:15 +0100

    updated layout

--- keyboards/niu_mini/keymaps/juju/keymap.c
+++ keyboards/niu_mini/keymaps/juju/keymap.c
@@ -7,17 +7,18 @@ enum {
   TD_QUOUTE_DOUBLE,
   TD_SLASH_QUESTION,
   TD_DOT_EXCLAIM,
-  TD_SLASH_PIPE
+  TD_SLASH_PIPE,
+  TD_MINUS_UNDER
 };
 
 // ---------------------------------------------------------------- Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-
   [TD_COLON_SEMI]  = ACTION_TAP_DANCE_DOUBLE(KC_COLON, KC_SCOLON),
   [TD_QUOUTE_DOUBLE]  = ACTION_TAP_DANCE_DOUBLE(KC_QUOTE, KC_DOUBLE_QUOTE),
   [TD_SLASH_QUESTION]  = ACTION_TAP_DANCE_DOUBLE(KC_SLASH, KC_QUESTION),
   [TD_DOT_EXCLAIM]  = ACTION_TAP_DANCE_DOUBLE(KC_DOT, KC_EXCLAIM),
   [TD_SLASH_PIPE]  = ACTION_TAP_DANCE_DOUBLE(KC_BSLASH, KC_PIPE),
+  [TD_MINUS_UNDER]  = ACTION_TAP_DANCE_DOUBLE(KC_MINUS, KC_UNDERSCORE),
 };
 
 // ----------------------------------------------------------------
@@ -69,54 +70,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |L1 ESC|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |Shift(|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   ?  |Shift)|
+     * |Shift(|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift)|
      * |------+------+------+------+------+------+------+------+------+------+------+------|  
-     * |Ctrl[ |  GUI |  Alt |      |Layer2|    Space    |Layer2|  `   |Quote |   /  |Hyper]|
+     * |Ctrl[ |   -  |  GUI |  Alt |Layer2|    Space    |Layer2|  `   |Quote |   \  |Hyper]|
      * `-----------------------------------------------------------------------------------'
-     */
-    [0]   = LAYOUT_planck_mit(
-      KC_TAB,                         KC_Q,    KC_W,    KC_E,       KC_R,  KC_T,   KC_Y,   KC_U,     KC_I,                 KC_O,                  KC_P,                  KC_BSPACE,
-      LT(1, KC_ESCAPE),               KC_A,    KC_S,    KC_D,       KC_F,  KC_G,   KC_H,   KC_J,     KC_K,                 KC_L,     TD(TD_COLON_SEMI),                   KC_ENTER,
-      KC_LSPO, 	                      KC_Z,    KC_X,    KC_C,       KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM,   TD(TD_DOT_EXCLAIM), TD(TD_SLASH_QUESTION),                    KC_RSPC,
-      LCTL_T(KC_LEFT_CURLY_BRACE), KC_LGUI, KC_LALT,    _______,   TT(2),     KC_SPC,     TT(2), KC_GRAVE, TD(TD_QUOUTE_DOUBLE),     TD(TD_SLASH_PIPE), MEH_T(KC_RIGHT_CURLY_BRACE)
+     */  
+    [0] = LAYOUT_planck_mit(
+      KC_TAB,                                    KC_Q,    KC_W,     KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,     KC_I,                 KC_O,                  KC_P,                  KC_BSPACE,
+      LT(1, KC_ESCAPE),                          KC_A,    KC_S,     KC_D,  KC_F,  KC_G,   KC_H,   KC_J,     KC_K,                 KC_L,     TD(TD_COLON_SEMI),                   KC_ENTER,
+      KC_LSPO, 	                                 KC_Z,    KC_X,     KC_C,  KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM,   TD(TD_DOT_EXCLAIM), TD(TD_SLASH_QUESTION),                    KC_RSPC,
+      LCTL_T(KC_LEFT_CURLY_BRACE), TD(TD_MINUS_UNDER), KC_LGUI,  KC_LALT, TT(2),     KC_SPC,     TT(2), KC_GRAVE, TD(TD_QUOUTE_DOUBLE),     TD(TD_SLASH_PIPE), MEH_T(KC_RIGHT_CURLY_BRACE)
     ), 
 
 
     /* Layer 1
     * ,-----------------------------------------------------------------------------------.
     * |      | ENTER| !+ESC| !ESC |  ^L  |  ^W  |      | HOME | PGDN | PGUP | END  |DELETE|
-    * |------+------+------+------+------+------+-------------+------+------+------+------|
-    * |      | ^A   |  ^S  |  DEL | BKSP |  ^P  |      | LEFT | DOWN | UP   | RIGHT|  RUN |
+    * |------+------+------+------+------+------+-------------+------+------+------+------| 
+    * |      | ^A   |  ^S  |  DEL | BKSP |  ^F  |      | LEFT | DOWN | UP   | RIGHT|  RUN |
     * |------+------+------+------+------+------+------|------+------+------+------+------|
-    * |      | ^Z   | ^X   | ^C   | ^V   | ^+W  |      |      |      |      |      |      |
+    * |      | ^Z   | ^X   | ^C   | ^V   | ^+P  |      |      |      |      |      |      |
     * |------+------+------+------+------+------+------+------+------+------+------+------|
-    * | RESET| ^+Z  |      |      |      |      |      |      |      |      |      |      |
+    * | RESET| ^+Z  |      |      |      |     ^P      |      |      |      |      |      |
     * `-----------------------------------------------------------------------------------'
     */
     [1] = LAYOUT_planck_mit(
-      _______,         KC_ENTER, LALT(LSFT(KC_ESC)), LALT(KC_ESC),  LCTL(KC_L),       LCTL(KC_T), _______, KC_HOME, KC_PGDOWN, KC_PGUP,   KC_END, KC_DELETE,
-      _______,       LCTL(KC_A),         LCTL(KC_S),    KC_DELETE,   KC_BSPACE,       LCTL(KC_P), _______, KC_LEFT,   KC_DOWN,   KC_UP, KC_RIGHT, LALT(KC_SPACE),
-      _______,       LCTL(KC_Z),         LCTL(KC_X),   LCTL(KC_C),  LCTL(KC_V), LCTL(LSFT(KC_P)), _______, _______,   _______, _______,  _______, _______,
-        RESET, LCTL(LSFT(KC_Z)),            _______,      _______,     _______,          _______,          _______,   _______, _______,  _______, _______ 
+      _______,         KC_ENTER, LALT(LSFT(KC_ESC)), LALT(KC_ESC),  LCTL(KC_L),       LCTL(KC_W), _______, KC_HOME, KC_PGDOWN, KC_PGUP,   KC_END,      KC_DELETE,
+      _______,       LCTL(KC_A),         LCTL(KC_S),    KC_DELETE,   KC_BSPACE,       LCTL(KC_F), _______, KC_LEFT,   KC_DOWN,   KC_UP, KC_RIGHT, LALT(KC_SPACE),
+      _______,       LCTL(KC_Z),         LCTL(KC_X),   LCTL(KC_C),  LCTL(KC_V), LCTL(LSFT(KC_P)), _______, _______,   _______, _______,  _______,        _______,
+        RESET, LCTL(LSFT(KC_Z)),            _______,      _______,     _______,            LCTL(KC_P),     _______,   _______, _______,  _______,        _______ 
     ),   
 
     /* Layer 2
     * ,-----------------------------------------------------------------------------------.
-    * |      |  F10  | F11 |  F12 |      |   !  |   $  |      |   7  |   8  |  9   |   +  |
+    * |      |  F10  | F11 |  F12 |      |   #  |   $  |  *   |   7  |   8  |  9   | BSPA |
     * |------+------+------+------+------+-------------+------+------+------+------+------|
-    * |      |  F7  |  F8  |  F9  |      |   "  |   %  |      |   4  |   5  |  6   |   -  |
+    * |      |  F7  |  F8  |  F9  |      |   ^  |   %  |  -   |   4  |   5  |  6   |Enter |
     * |------+------+------+------+------+------|------+------+------+------+------+------|
-    * |      |  F4  |  F5  |  F6  |      |   #  |   ^  |      |   1  |   2  |  3   |   *  |
+    * |      |  F4  |  F5  |  F6  |      |      |      |  +   |   1  |   2  |  3   |      |
     * |------+------+------+------+------+------+------+------+------+------+------+------|
     * |      |  F1  |  F2  |  F3  |      |      |      |      |   0  |   .  |   =  |   /  |
     * `-----------------------------------------------------------------------------------' 
     */
     [2] = LAYOUT_planck_mit(
-      _______, KC_F10, KC_F11, KC_F12, _______,    KC_EXCLAIM,     KC_DOLLAR, _______, KC_7,   KC_8,         KC_9,     KC_KP_PLUS,
-      _______, KC_F7,   KC_F8,  KC_F9, _______,      KC_QUOTE,    KC_PERCENT, _______, KC_4,   KC_5,         KC_6,    KC_KP_MINUS,
-      _______, KC_F4,   KC_F5,  KC_F6, _______, KC_NONUS_HASH, KC_CIRCUMFLEX, _______, KC_1,   KC_2,         KC_3, KC_KP_ASTERISK,
-      _______, KC_F1,   KC_F2,  KC_F3, _______,            _______,           _______, KC_0, KC_DOT,  KC_KP_EQUAL,    KC_KP_SLASH
-    )    
+      _______, KC_F10, KC_F11, KC_F12, _______, KC_NONUS_HASH,  KC_DOLLAR,     KC_KP_PLUS,  KC_7,   KC_8,        KC_9,   KC_BSPACE, 
+      _______, KC_F7,   KC_F8,  KC_F9, _______, KC_CIRCUMFLEX, KC_PERCENT,    KC_KP_MINUS,  KC_4,   KC_5,        KC_6,    KC_ENTER,
+      _______, KC_F4,   KC_F5,  KC_F6, _______,       _______,    _______, KC_KP_ASTERISK,  KC_1,   KC_2,        KC_3,     _______, 
+      _______, KC_F1,   KC_F2,  KC_F3, _______,            _______,               _______,  KC_0, KC_DOT, KC_KP_EQUAL, KC_KP_SLASH
+    )
 
 };
 
