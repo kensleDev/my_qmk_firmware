6e2e4f9fe3bd557e2407e5fe40d71f1a29969b5c
kensleDev <kensledev@gmail.com>
2020-12-12 17:27:31 +0000

    :hammer: updated keymap

--- keyboards/keebio/nyquist/keymaps/kensleDev2/keymap.c
+++ keyboards/keebio/nyquist/keymaps/kensleDev2/keymap.c
@@ -4,7 +4,8 @@ enum layer_number {
     _WIN = 0,
     _MAC,
     _LOWER,
-    _HIGHER
+    _HIGHER,
+    _FUNCT
 };
 
 /// ------------------------------------------------  variables
@@ -14,6 +15,7 @@ enum layer_number {
 #define LOWER LT(_LOWER, KC_BSPC)
 #define HIGHER LT(_HIGHER, KC_SPC)
 #define NAV LT(_NAV, KC_ESC)
+#define FUNCT LT(_FUNCT, KC_F13)
 
 #define SFT_Z LSFT_T(KC_Z)
 #define SFT_SLH LSFT_T(KC_SLSH)
@@ -21,8 +23,8 @@ enum layer_number {
 #define CTLETR MT(MOD_LCTL, KC_ENT)
 #define CTLESC MT(MOD_LCTL, KC_ESC)
 #define CMDETR MT(MOD_LGUI, KC_ENT)
+#define WINESC MT(MOD_LGUI, KC_ESC)
 
-#define LAUNCH LCTL(KC_F7)
 
 
 // Window management
@@ -39,6 +41,8 @@ enum layer_number {
 #define WL_FD A(KC_SPACE)
 #define WL_BK S(A(KC_SPACE))
 #define WL_MENU A(KC_P)
+
+#define ALTTAB A(KC_TAB)
 
 
 
@@ -77,13 +81,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
         break;
-        case MAC_KEY:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_MAC);
-            }
-            return false;
-        break;
-    };
+   };
     return true;
 }
 
@@ -97,18 +95,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // <!-- keymap -->
 
-[_MAC] = LAYOUT_ortho_4x12( \
-//┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    XXXXXXX, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , XXXXXXX,
-//├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-    XXXXXXX, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, XXXXXXX,
-//├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-    XXXXXXX, SFT_Z  , KC_X   , KC_C   , KC_V   , KC_B   ,   KC_N   , KC_M   , KC_COMM, KC_DOT , SFT_SLH, XXXXXXX,
-//├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-    XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT, LOWER  , CMDETR ,   CTLESC , HIGHER , LAUNCH , XXXXXXX, XXXXXXX, XXXXXXX
-//└────────┴────────┴────────┴────────┴────────┴────────┘ └────────┴────────┴────────┴────────┴────────┴────────┘
-),
-
 [_WIN] = LAYOUT_ortho_4x12( \
 //┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐
     XXXXXXX, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , XXXXXXX,
@@ -117,19 +103,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
     XXXXXXX, SFT_Z  , KC_X   , KC_C   , KC_V   , KC_B   ,   KC_N   , KC_M   , KC_COMM, KC_DOT , SFT_SLH, XXXXXXX,
 //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-    XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT, LOWER  , CTLETR ,   _______, HIGHER , LAUNCH , XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, FUNCT  , LOWER  , CTLETR ,   WINESC , HIGHER , KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX
 //└────────┴────────┴────────┴────────┴────────┴────────┘ └────────┴────────┴────────┴────────┴────────┴────────┘
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12( \
 //┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐
-    XXXXXXX, W_X    , _______, W_SWP  , W_PRM  , _______,   _______, KC_7   , KC_8   , KC_9   , KC_PLUS, XXXXXXX,
+    XXXXXXX, _______, _______, _______, _______, _______,   _______, KC_7   , KC_8   , KC_9   , KC_PLUS, XXXXXXX,
 //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-    XXXXXXX, W_SM   , W_BK   , W_FD   , W_BG   , KC_TAB ,   _______, KC_4   , KC_5   , KC_6   , KC_MINS, XXXXXXX,
+    XXXXXXX, _______, _______, _______, _______, KC_TAB ,   ALTTAB , KC_4   , KC_5   , KC_6   , KC_MINS, XXXXXXX,
 //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-    XXXXXXX, WL_RST , WL_BK  , WL_FD  , WL_MENU, KC_DEL ,   KC_EQL , KC_1   , KC_2   , KC_3   , KC_ASTR, XXXXXXX,
+    XXXXXXX, _______, _______, _______, _______, KC_DEL ,   KC_EQL , KC_1   , KC_2   , KC_3   , KC_ASTR, XXXXXXX,
 //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-    XXXXXXX, XXXXXXX, XXXXXXX, MAC_KEY, _______, WIN_KEY,   _______, RESET  , KC_0   , XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______,   _______, KC_0   , KC_EQL , XXXXXXX, XXXXXXX, XXXXXXX
 //└────────┴────────┴────────┴────────┴────────┴────────┘ └────────┴────────┴────────┴────────┴────────┴────────┘
 ),
 
@@ -141,8 +127,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
     XXXXXXX, KC_PIPE, KC_LBRC, KC_LCBR, KC_LPRN, KC_AMPR,   KC_BSLS, KC_RPRN, KC_RCBR, KC_RCBR, KC_TILD, XXXXXXX,
 //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
-    XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_BTN4, KC_BTN5,   _______, _______, LAUNCH , XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, _______, KC_BTN4, KC_BTN5,   _______, FUNCT  , RESET  , XXXXXXX, XXXXXXX, XXXXXXX
+//└────────┴────────┴────────┴────────┴────────┴────────┘ └────────┴────────┴────────┴────────┴────────┴────────┘
+),
+
+[_FUNCT] = LAYOUT_ortho_4x12( \
+//┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐
+    XXXXXXX, _______, _______, _______, _______, _______,   _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , XXXXXXX,
+//├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
+    XXXXXXX, _______, _______, _______, _______, _______,   _______, KC_F4  , KC_F5  , KC_F6  , KC_F11 , XXXXXXX,
+//├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
+    XXXXXXX, _______, _______, _______, _______, _______,   _______, KC_F1  , KC_F2  , KC_F3  , KC_F12 , XXXXXXX,
+//├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
+    XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______,   _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
 //└────────┴────────┴────────┴────────┴────────┴────────┘ └────────┴────────┴────────┴────────┴────────┴────────┘
 ),
 // <!-- keymap end -->
 };
+
+
+
+/* [_MAC] = LAYOUT_ortho_4x12( \ */
+/* //┌────────┬────────┬────────┬────────┬────────┬────────┐ ┌────────┬────────┬────────┬────────┬────────┬────────┐ */
+/*     XXXXXXX, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , XXXXXXX, */
+/* //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤ */
+/*     XXXXXXX, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, XXXXXXX, */
+/* //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤ */
+/*     XXXXXXX, SFT_Z  , KC_X   , KC_C   , KC_V   , KC_B   ,   KC_N   , KC_M   , KC_COMM, KC_DOT , SFT_SLH, XXXXXXX, */
+/* //├────────┼────────┼────────┼────────┼────────┼────────┤ ├────────┼────────┼────────┼────────┼────────┼────────┤ */
+/*     XXXXXXX, XXXXXXX, XXXXXXX, FUNC   , LOWER  , CMDETR ,   CTLESC , HIGHER , KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX */
+/* //└────────┴────────┴────────┴────────┴────────┴────────┘ └────────┴────────┴────────┴────────┴────────┴────────┘ */
+/* ), */
+
+        /* case MAC_KEY: */
+        /*     if (record->event.pressed) { */
+        /*         set_single_persistent_default_layer(_MAC); */
+        /*     } */
+        /*     return false; */
+        /* break; */
+ 
